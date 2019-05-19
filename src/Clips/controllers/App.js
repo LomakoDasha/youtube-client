16a6434b5d6b4677b1268522f908d48523c59a3d
Dasha <lomakodasha@gmail.com>
2019-05-19 19:02:50 +0300

    refactor: delete comments

--- src/Clips/controllers/App.js
+++ src/Clips/controllers/App.js
@@ -10,16 +10,10 @@ export default class App {
     const state = {
       url: `https://www.googleapis.com/youtube/v3/videos?key=AIzaSyBOFwKCUVk7f6mLMO4khwLq_8ehtXsYiJo&id=${this.id}&part=snippet,statistics`,
     };
-    // eslint-disable-next-line no-console
-    console.log('this.id', this.id);
-    // eslint-disable-next-line no-console
-    console.log('state.url', state.url);
 
     const model = new AppModel(state);
     const data = await model.getData();
 
-    // eslint-disable-next-line no-console
-    console.log('data', data);
     const view = new AppView(data);
 
     view.render();
